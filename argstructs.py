23053ca50f29300dcacd71a495ffea41af43dd82
Tom <tom@contextfree.info>
2021-08-26 22:10:38 -0700

    Consider struct vs function

--- argstructs.py
+++ argstructs.py
@@ -1,6 +1,12 @@
 import dataclasses as dc
 
+
 @dc.dataclass
-class ListFormatOptions:
-    ends: str
-    sep: str
+class IntListFormatting:
+    sep: str = ","
+    items: list[int] = dc.field(default_factory=list)
+    ends: str = "[]"
+
+
+def format_int_list(sep=",", *items: int, ends: str):
+    pass
