d0386230babba9a8eda1e2cc1f437a2d78dd3e9d
Tom <tom@contextfree.info>
2021-08-28 00:00:10 -0700

    Add named and defaults to hs

--- argstructs.hs
+++ argstructs.hs
@@ -1,9 +1,18 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 import Data.List
 import Text.Printf
 
+data ListingArgs a = ListingArgs {items :: [a], sep :: String, ends :: String}
+
+listingArgsDefaults :: ListingArgs a
+listingArgsDefaults = ListingArgs {items=[], sep=", ", ends="[]"}
+
+-- listing :: Show a => ListingArgs a -> String
+-- listing ListingArgs {items, sep, ends} =
 -- listing :: Show a => ([a], String, String) -> String
 -- listing (items, sep, ends) =
-listing :: Show a => [a] -> String -> String -> String
+-- listing :: Show a => [a] -> String -> String -> String
 listing items sep ends =
     printf "%s%s%s" begin (intercalate sep $ map show items) end
     where
@@ -13,3 +22,5 @@ listing items sep ends =
 main =
     putStrLn $ listing [1, 2, 3] ", " "[]"
     -- putStrLn $ listing ([1, 2, 3], ", ", "[]")
+    -- putStrLn $ listing ListingArgs {items=[1, 2, 3], sep=", ", ends="[]"}
+    -- putStrLn $ listing listingArgsDefaults {items=[1, 2, 3]}
