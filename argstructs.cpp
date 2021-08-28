a9c1bdce2be3148d564218f00dc7136b01ef61b6
Tom <tom@contextfree.info>
2021-08-28 09:54:57 -0700

    Provide uniform list in cpp

--- argstructs.cpp
+++ argstructs.cpp
@@ -1,6 +1,25 @@
+#include <initializer_list>
 #include <iostream>
+#include <ranges>
 #include <sstream>
 #include <string_view>
+
+template <typename Item>
+auto listing(std::initializer_list<Item> items, std::string_view sep,
+             std::string_view ends) -> std::string {
+  auto buffer = std::stringstream();
+  auto begin = ends.size() ? ends.substr(0, 1) : "";
+  auto end = ends.size() > 1 ? ends.substr(1, 1) : "";
+  buffer << begin;
+  if (items.size()) {
+    buffer << *items.begin();
+    for (const auto& item : items | std::views::drop(1)) {
+      buffer << sep << item;
+    }
+  }
+  buffer << end;
+  return buffer.str();
+}
 
 auto join(std::stringstream& buffer, std::string_view sep) -> void {}
 
@@ -21,10 +40,12 @@ auto listing(std::string_view sep, std::string_view ends, const Items&... items)
   join(buffer, sep, items...);
   buffer << end;
   return buffer.str();
+  // return listing({items...}, sep, ends);
 }
 
 auto main() -> int {
-  auto text = listing(", ", "[]", 1, 2, 3);
+  auto text = listing({1, 2, 3}, ", ", "[]");
+  // auto text = listing(", ", "[]", 1, 2, 3, "last");
   // auto text = listing(", ", "[]");
   std::cout << text << "\n";
 }
