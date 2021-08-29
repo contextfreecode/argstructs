7e0bcfad504940dd0ed2bb161547e104a25af259
Tom <tom@contextfree.info>
2021-08-28 22:35:32 -0700

    Clean up some

--- argstructs.hs
+++ argstructs.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, NamedFieldPuns, UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, NamedFieldPuns, UndecidableInstances #-}
 
 import Data.List
 import Text.Printf
@@ -13,13 +13,13 @@ instance {-# OVERLAPS #-} Show' String where
     show' = id
 
 class Join r where
-    join :: (Show' a) => String -> a -> r
+    join :: (Show a, Show' a) => String -> a -> r
 
 instance Join String where
     join sep = show'
 
-instance (Show' a, Join r) => Join (a -> r) where
-    join sep acc x = join sep (show' acc ++ sep ++ show' x)
+instance (Show a, Show' a, Join r) => Join (a -> r) where
+    join sep acc x = join sep (show' acc ++ sep ++ show x)
 
 data ListingArgs a = ListingArgs {items :: [a], sep :: String, ends :: String}
 
@@ -35,11 +35,11 @@ listing items sep ends =
     printf "%s%s%s" begin (intercalate sep $ map show items) end
     where
         begin = take 1 ends
-        end = drop 1 ends
+        end = take 1 $ drop 1 ends
 
 main = putStrLn $
-    -- listing [1, 2, 3] ";" "[]"
+    listing [1, 2, 3] ";" "[]"
     -- listing ([1, 2, 3], " ", "()")
     -- listing ListingArgs {items=[1, 2, 3], sep=" : ", ends=""}
     -- listing listingArgsDefaults {items=[1, 2, 3]}
-    join ";" (1 :: Int) (2 :: Int) (3 :: Int) "last"
+    -- join ";" (1 :: Int) (2 :: Int) (3 :: Int) "last"
