8562af4df70bfa3600415fdf9058921eb5478a5d
Tom <tom@contextfree.info>
2021-08-30 06:42:04 -0700

    Churn python

--- argstructs.py
+++ argstructs.py
@@ -21,13 +21,14 @@ import typing as tp
 #     ends: str
 
 
-# ListingArgs = tuple[list, str, str]
+ListingArgs = tuple[list, str, str]
 
 
-# def listing(sep=", ", *items: int, ends: str = "[]") -> str:
-# def listing(*args: ListingArgs) -> str:
-def listing(items: list, sep: str, ends: str) -> str:
-    # items, sep, ends = args
+# def listing(*items, sep=", ", ends: str = "[]") -> str:
+# def listing(sep, *items, ends: str = "[]") -> str:
+# def listing(*args) -> str:
+def listing(items: list, sep: str = ", ", ends: str = "[]") -> str:
+    # items, sep, ends = tp.cast(ListingArgs, args)
     begin = ends[:1]
     end = ends[1:2]
     joined = sep.join(str(i) for i in items)
@@ -35,21 +36,22 @@ def listing(items: list, sep: str, ends: str) -> str:
 
 
 def main():
-    text = listing([1, 2, 3], ", ", "[]")
-    # text = listing ([1, 2, 3], ", ", "[]")
-    # text = listing(*([1, 2, 3], ", ", "[]"))
-    # text = listing(*tp.cast(ListingArgs, ([1, 2, 3], ", ", "[]")))
-    # text = listing(*ListingArgs([1, 2, 3], ", ", "[]"))
-    # text = listing(items=[1, 2, 3], sep=", ", ends="[]")
-    # text = listing(*ListingArgs(items=[1, 2, 3], sep=", ", ends="[]"))
-    # text = listing(**{"items": [1, 2, 3], "sep": ", ", "ends": "[]"})
-    # text = listing(**dict(items=[1, 2, 3], sep=", ", ends="[]"))
-    # text = listing(**ListingArgs(items=[1, 2, 3], sep=", ", ends="[]")._asdict())
-    # text = listing(**ListingArgs(items=[1, 2, 3], sep=", ", ends="[]"))
-    # text = listing(**ListingArgs(items=[1, 2, 3], sep=", ", ends="[]").__dict__)
-    # text = listing(", ", 1, 2, 3, ends="[]")
-    # text = listing(sep=", ", 1, 2, 3, ends="[]")
-    # text = listing(1, 2, 3, sep=", ", ends="[]")
+    text = listing([1, 2, 3], " ", "[]")
+    # text = listing ([1, 2, 3], "/", "[]")
+    # text = listing(*([1, 2, 3], "; ", "[]"))
+    # text = listing(*tp.cast(ListingArgs, ([1, 2, 3], " ", "()")))
+    # text = listing(*ListingArgs([1, 2, 3], "; ", "()"))
+    # text = listing(items=[1, 2, 3], sep=", ", ends="()")
+    # text = listing(*ListingArgs(items=[1, 2, 3], sep="/", ends="()"))
+    # text = listing(**{"items": [1, 2, 3], "sep": " ", "ends": "<>"})
+    # text = listing(**dict(items=[1, 2, 3], sep=", ", ends="<>"))
+    # text = listing(**ListingArgs(items=[1, 2, 3], sep="; ", ends="<>")._asdict())
+    # text = listing(**ListingArgs(items=[1, 2, 3], sep="/", ends="<>"))
+    # text = listing(**ListingArgs(items=[1, 2, 3], sep=", ", ends="").__dict__)
+    # text = listing(" ", 1, 2, 3, ends="")
+    # text = listing(sep=":", 1, 2, 3, ends="")
+    # text = listing(1, 2, 3, sep="/", ends="")
+    # text = listing(1, 2, 3)
     print(text)
 
 
