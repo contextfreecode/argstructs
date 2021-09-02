68dfec2b7a0d1adaebcebda0c478338c6cfe7dc8
Tom <tom@contextfree.info>
2021-09-02 05:40:13 -0700

    Tweak ts

--- argstructs.ts
+++ argstructs.ts
@@ -1,23 +1,24 @@
-// class ListingArgs {
-//   constructor(
-//     items: unknown[],
-//     sep?: string,
-//     ends?: string,
-//   ) {
-//     this.items = items;
-//     if (sep != null) this.sep = sep;
-//     if (ends != null) this.ends = ends;
-//   }
-//   items: unknown[];
-//   sep = ", ";
-//   ends = "[]";
-// }
-// const listIntDefaults = Object.freeze({sep: ", ", ends: "[]"});
+class ListingArgsClass {
+  constructor(
+    items: unknown[],
+    sep?: string,
+    ends?: string,
+  ) {
+    this.items = items;
+    if (sep != undefined) this.sep = sep;
+    if (ends != undefined) this.ends = ends;
+  }
+  items: unknown[];
+  sep = ", ";
+  ends = "[]";
+}
+const listIntDefaults = Object.freeze({sep: ", ", ends: "[]"});
 // type ListingArgs = {items: unknown[], sep: string, ends: string};
 // type NumberArray = number[];  // or Array<number>
 // type ListingArgs = [...items: unknown[], sep: string, ends: string];
-type ListingArgs = [sep: string, ends: string, ...items: unknown[]];
-// type ListingArgs = [items: unknown[], sep: string, ends: string];
+// type ListingArgs = [sep: string, ends: string, ...items: unknown[]];
+// type ListingArgs = [items: unknown[], sep?: string, ends?: string];
+type ListingArgs = [items: unknown[], sep: string, ends: string];
 
 // function listing({items, sep, ends}: ListingArgs): string {
 // function listing(...[items, sep = ", ", ends = "[]"]: ListingArgs): string {
@@ -25,7 +26,7 @@ type ListingArgs = [sep: string, ends: string, ...items: unknown[]];
 // function listing(sep = ", ", ends = "[]", items: unknown[]): string {
 // function listing(items: unknown[], sep = ", ", ends = "[]"): string {
 // function listing(...items: unknown[], sep: string, ends = "[]"): string {
-// function listing(sep: string, ends = "[]", ...items: unknown[]): string {
+// function listing(sep: string = ", ", ends = "[]", ...items: unknown[]): string {
 function listing(items: unknown[], sep: string, ends: string): string {
   const begin = ends.slice(0, 1);
   const end = ends.slice(1, 2);
@@ -34,15 +35,16 @@ function listing(items: unknown[], sep: string, ends: string): string {
 }
 
 function main() {
-  const text = listing([1, 2, 3], ", ", "[]");
-  // const text = listing(", ", "[]", 1, 2, 3);
-  // const text = listing(...[", ", "[]", 1, 2, 3] as ListingArgs);
-  // const text = listing(", ");
+  const text = listing([1, 2, 3], ", ", "()");
+  // const text = listing(" ", "()", 1, 2, 3);
+  // const text = listing(undefined, undefined, 1, 2, 3);
+  // const text = listing(...["; ", "()", 1, 2, 3] as ListingArgs);
   // const text = listing();
-  // const text = listing(new ListingArgs([1, 2, 3]));
   // const text = listing([1, 2, 3]);
-  // const text = listing(...[[1, 2, 3], ", ", "[]"] as ListingArgs);
-  // const text = listing({items: [1, 2, 3], sep: ", ", ends: "[]"});
+  // const text = listing(...[[1, 2, 3], " ", ""] as ListingArgs);
+  // const [a, b, c, sep, ends] = [1, 2, 3, " ", ""] as ListingArgs;
+  // const text = listing({items: [1, 2, 3], sep: "; ", ends: ""});
+  // const text = listing(new ListingArgsClass([1, 2, 3]));
   // const text = listing({...listIntDefaults, items: [1, 2, 3]});
   console.log(text);
 }
